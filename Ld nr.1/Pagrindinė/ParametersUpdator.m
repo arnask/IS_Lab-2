ee0b79d6b760b2340a07de23e7be9039cbe32ad5
Arnas <arnaskar@gmail.com>
2020-09-23 22:32:59 +0300

    First

--- Ld nr.1/Pagrindinė/ParametersUpdator.m
+++
@@ -1,8 +0,0 @@
-function [W1, W2, B] = ParametersUpdator(W1, W2, B, X1, X2, lr, e)
-    for i = 1 : length(X1)
-    W1 = W1 + lr * e(i) * X1(i);
-    W2 = W2 + lr * e(i) * X2(i);
-    B = B + lr * e(i);
-    end
-end
-
